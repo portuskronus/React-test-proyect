1ebc60d42c2c35b235be6684f2f5f9f01a5859f5
GiulliaFerraro <98989747+GiulliaFerraro@users.noreply.github.com>
2022-09-22 09:49:24 -0400

    Update Navbar.jsx

--- crypto-proyect/src/components/Navbar.jsx
+++ crypto-proyect/src/components/Navbar.jsx
@@ -1,30 +1,30 @@
 import React from "react";
-import { useState } from "react";
+
 import { navLinks } from "../constants/index";
 import Button from "./Button";
 
 const Navbar = () => {
-  const [active, setActive] = useState("Home");
   return (
-
-   <nav className="w-full flex py-4 justify-between items-center bg-black bg-opacity-70">
-        <ul className="list-none flex  items-center flex-1">
-          {navLinks.map((nav, index) => (
-            <li
-              key={nav.id}
-              className={`font-gafata font-thin text-yellow-600 hover:text-yellow-300 cursor-pointer text-[16px]
+    <nav className="w-full flex py-4 justify-between items-center navbar bg-black bg-opacity-60 ">
+      <ul className="list-none flex  items-center flex-1">
+        {navLinks.map((nav, index) => (
+          <li
+            key={nav.id}
+            className={`font-gafata font-thin text-yellow-600 hover:text-yellow-300 cursor-pointer text-[16px]
           
                ${index === navLinks.length - 1 ? "mr-0" : "mr-5"}`}
-              onClick={() => setActive(nav.title)}
-            >
-              <a href={`#${nav.id}`}>{nav.title}</a>
-            </li>
-          ))}
-        </ul>
-        <div> <Button/></div>
-      </nav>
-  
-    )
+          >
+            <a href={`#${nav.id}`}>{nav.title}</a>
+          </li>
+        ))}
+      </ul>
+      
+      <div>
+        {" "}
+        <Button />
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
